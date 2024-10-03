e4ee3b0925675b245519b670685169011339df15
candycx20 <candycarrillo060@gmail.com>
2024-10-03 13:32:59 -0600

    Corregir URL

--- src/components/shopCartandCheckout/Checkout.jsx
+++ src/components/shopCartandCheckout/Checkout.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-const URL = "http://localhost:2003/";
+const URL = "http://candy21.icu/";
 
 const countries = [
   "Australia",
